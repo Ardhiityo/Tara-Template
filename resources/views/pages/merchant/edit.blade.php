d10945dbf40805735813466658043a03917c05c6
belajar-vue-2 <aryaadi229@gmail.com>
2025-08-30 14:57:15 +0700

    fix:status form merchant edit

--- resources/views/pages/merchant/edit.blade.php
+++ resources/views/pages/merchant/edit.blade.php
@@ -19,11 +19,17 @@
                             value="Yani MUA" required">
                     </div>
                     <div class="w-full">
-                        <label for="email"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="email" name="email" id="email"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            value="yani@gmail.com" required readonly>
+                        <label for="status"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+                        <select id="type"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="status">
+                            <option selected value="">Choose a country</option>
+                            <option value="active">Active</option>
+                            <option value="inactive">Inactive</option>
+                            <option value="suspend">Suspend</option>
+                            <option value="pending">Pending</option>
+                        </select>
                     </div>
                     <div class="w-full">
                         <label for="phone"
